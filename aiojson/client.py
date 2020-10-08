6ad5d6eb9b1c7948b22acfb69dac76671f712057
Yurzs <yurzs@icloud.com>
2020-10-08 23:57:25 +0300

    client: allow headers passing to make_request

--- aiojson/client.py
+++ aiojson/client.py
@@ -19,34 +19,34 @@ class ApiClient:
     def _format_path(self, endpoint):
         return f"{self.protocol}://{self.server_ip}:{self.server_port}/{endpoint}"
 
-    async def _make_request(self, endpoint, method, json_data=None, keep_none=False):
+    async def _make_request(self, endpoint, method, json_data=None, keep_none=False, headers=None):
         url = self._format_path(endpoint)
         data = self._delete_none(json_data or {}) if not keep_none else json_data or {}
         async with aiohttp.ClientSession() as session:
             _method = getattr(session, method)
-            async with _method(url, json=data) as response:
+            async with _method(url, json=data, headers=headers) as response:
                 result = await response.json()
                 if result["error"]:
                     raise ApiException(result["reason"].get("text", result["reason"]))
                 else:
                     return result["result"]
 
-    async def get(self, endpoint, json_data=None, keep_none=False):
+    async def get(self, endpoint, json_data=None, keep_none=False, headers=None):
         return await self._make_request(endpoint, "get", json_data=json_data,
-                                        keep_none=keep_none)
+                                        keep_none=keep_none, headers=headers)
 
-    async def post(self, endpoint, json_data=None, keep_none=False):
+    async def post(self, endpoint, json_data=None, keep_none=False, headers=None):
         return await self._make_request(endpoint, "post", json_data=json_data,
-                                        keep_none=keep_none)
+                                        keep_none=keep_none, headers=headers)
 
-    async def delete(self, endpoint, json_data=None, keep_none=False):
+    async def delete(self, endpoint, json_data=None, keep_none=False, headers=None):
         return await self._make_request(endpoint, "delete", json_data=json_data,
-                                        keep_none=keep_none)
+                                        keep_none=keep_none, headers=headers)
 
-    async def put(self, endpoint, json_data=None, keep_none=False):
+    async def put(self, endpoint, json_data=None, keep_none=False, headers=None):
         return await self._make_request(endpoint, "put", json_data=json_data,
-                                        keep_none=keep_none)
+                                        keep_none=keep_none, headers=headers)
 
-    async def patch(self, endpoint, json_data=None, keep_none=False):
+    async def patch(self, endpoint, json_data=None, keep_none=False, headers=None):
         return await self._make_request(endpoint, "patch", json_data=json_data,
-                                        keep_none=keep_none)
+                                        keep_none=keep_none, headers=headers)
